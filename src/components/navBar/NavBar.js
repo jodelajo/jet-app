97b3243e145e6f2abd0c7b7d9217964ec3d21256
jodelajo <jodelajo@gmail.com>
2021-07-25 15:57:11 +0200

    mobile nav

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,16 +1,115 @@
-import React from "react"
-import styles from './NavBar.module.css'
+import React, { useState } from "react";
+import styles from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return (<div className={styles.navBar}>
-        <div className={styles.name}>Vrolijke Noo<span className={styles.noot}>♪</span>zaak</div>
-        <div className={styles.menu}>
-            <NavLink exact to="/" className={styles.link} activeClassName={styles.active}>Home</NavLink>
-            <NavLink to="/coaching" className={styles.link} activeClassName={styles.active}>Coaching en Mediation</NavLink>
-            <NavLink to="/werkwijze" className={styles.link} activeClassName={styles.active}>Werkwijze</NavLink>
-            <NavLink to="/over" className={styles.link} activeClassName={styles.active}>Persoon</NavLink>
-            <NavLink to="/contact" className={styles.link} activeClassName={styles.active}>Contact</NavLink>
-        </div>
-    </div>)
+  const [checked, setChecked] = useState(false);
+
+  function handleClick() {
+    setChecked(!checked);
+  }
+
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.name}>
+        Vrolijke Noo<span className={styles.noot}>♪</span>zaak
+      </div>
+
+      <label htmlFor="hamburger" className={styles.hamburgerIcon}>
+        &#9776;
+      </label>
+
+      <input
+        type="checkbox"
+        className={styles.hamburger}
+        id="hamburger"
+        checked={checked}
+        onChange={() => {
+          setChecked(!checked);
+        }}
+      />
+      {checked && (<div className={styles.menu}>
+        <NavLink
+          exact
+          to="/"
+          className={styles.link}
+          activeClassName={styles.active}
+          onClick={handleClick}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/coaching"
+          className={styles.link}
+          activeClassName={styles.active}
+          onClick={handleClick}
+        >
+          Coaching en Mediation
+        </NavLink>
+        <NavLink
+          to="/werkwijze"
+          className={styles.link}
+          activeClassName={styles.active}
+          onClick={handleClick}
+        >
+          Werkwijze
+        </NavLink>
+        <NavLink
+          to="/over"
+          className={styles.link}
+          activeClassName={styles.active}
+          onClick={handleClick}
+        >
+          Persoon
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={styles.link}
+          activeClassName={styles.active}
+          onClick={handleClick}
+        >
+          Contact
+        </NavLink>
+      </div>)}
+
+      {!checked && (<div className={styles.menuWeb}>
+        <NavLink
+          exact
+          to="/"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/coaching"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          Coaching en Mediation
+        </NavLink>
+        <NavLink
+          to="/werkwijze"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          Werkwijze
+        </NavLink>
+        <NavLink
+          to="/over"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          Persoon
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          Contact
+        </NavLink>
+      </div>)}
+    </div>
+  );
 }
